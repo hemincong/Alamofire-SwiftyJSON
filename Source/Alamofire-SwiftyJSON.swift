12651e2b60284e970b43388cdb77953ae7ed46c1
mincong.he <mincong.he@gmail.com>
2015-10-12 17:51:13 +0800

    update for Alamofire interface change

--- Source/Alamofire-SwiftyJSON.swift
+++ Source/Alamofire-SwiftyJSON.swift
@@ -18,9 +18,9 @@ extension Request {
     /**
     Adds a handler to be called once the request has finished.
     
-    - parameter completionHandler: A closure to be executed once the request has finished. The closure takes 4 arguments: the URL request, the URL response, if one was received, the SwiftyJSON enum, if one could be created from the URL response and data, and any error produced while creating the SwiftyJSON enum.
+    :param: completionHandler A closure to be executed once the request has finished. The closure takes 4 arguments: the URL request, the URL response, if one was received, the SwiftyJSON enum, if one could be created from the URL response and data, and any error produced while creating the SwiftyJSON enum.
     
-    - returns: The request.
+    :returns: The request.
     */
     public func responseSwiftyJSON(completionHandler: (NSURLRequest, NSHTTPURLResponse?, SwiftyJSON.JSON, ErrorType?) -> Void) -> Self {
         return responseSwiftyJSON(nil, options:NSJSONReadingOptions.AllowFragments, completionHandler:completionHandler)
@@ -29,28 +29,29 @@ extension Request {
     /**
     Adds a handler to be called once the request has finished.
     
-    - parameter queue: The queue on which the completion handler is dispatched.
-    - parameter options: The JSON serialization reading options. `.AllowFragments` by default.
-    - parameter completionHandler: A closure to be executed once the request has finished. The closure takes 4 arguments: the URL request, the URL response, if one was received, the SwiftyJSON enum, if one could be created from the URL response and data, and any error produced while creating the SwiftyJSON enum.
+    :param: queue The queue on which the completion handler is dispatched.
+    :param: options The JSON serialization reading options. `.AllowFragments` by default.
+    :param: completionHandler A closure to be executed once the request has finished. The closure takes 4 arguments: the URL request, the URL response, if one was received, the SwiftyJSON enum, if one could be created from the URL response and data, and any error produced while creating the SwiftyJSON enum.
     
-    - returns: The request.
+    :returns: The request.
     */
     public func responseSwiftyJSON(queue: dispatch_queue_t? = nil, options: NSJSONReadingOptions = .AllowFragments, completionHandler: (NSURLRequest, NSHTTPURLResponse?, JSON, ErrorType?) -> Void) -> Self {
         
-        return response(queue: queue, responseSerializer: Request.JSONResponseSerializer(options: options), completionHandler: { (request, response, result) -> Void in
+        return response(queue: queue, completionHandler: { (request, response, data, result) -> Void in
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
                 var responseJSON: JSON
-                if result.isFailure
-                {
+                if (result != nil) {
                     responseJSON = JSON.null
                 } else {
-                    responseJSON = SwiftyJSON.JSON(result.value!)
+                    responseJSON = SwiftyJSON.JSON(data: data!)
                 }
                 dispatch_async(queue ?? dispatch_get_main_queue(), {
-                    completionHandler(self.request!, self.response, responseJSON, result.error)
+                    completionHandler(self.request!, self.response, responseJSON, result)
                 })
             })
+
         })
+
     }
 }
 
